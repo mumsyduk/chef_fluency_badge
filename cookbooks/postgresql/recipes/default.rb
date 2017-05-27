456957056e3cd1147da39728f2a5eb70c192828b
kofi <kofi.akomeah@gmail.com>
2017-05-27 11:04:14 +0000

    add postgresql cookbook

---
+++ cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: postgresql
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#
+#
+#
+package 'postgresql-server' do
+         notifies :run, 'execute[postgresql-init]'
+end
+
+execute 'postgresl-init' do
+         command 'postgresl-setup initdb'
+         action :nothing
+end
+
+service 'postgresl' do
+         action [:enable, :start]
+end
