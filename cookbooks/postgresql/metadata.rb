f1e11a15a9d9172d89408bf6d7954a967ea9f6b2
kofi <kofi.akomeah@gmail.com>
2017-05-27 11:38:23 +0000

    changes to postgresql cookbook

--- cookbooks/postgresql/metadata.rb
+++ cookbooks/postgresql/metadata.rb
@@ -1,10 +1,10 @@
 name 'postgresql'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Kofi Akomeah'
+maintainer_email 'kofi.akomeah@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures postgresql'
 long_description 'Installs/Configures postgresql'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
